40d3ae3dc683ebb4ef9b3b91dd6ea2ea1e2057c2
williampai <williampai0704@gmail.com>
2023-07-14 14:32:36 +0200

    add arap()

--- ARAP.cpp
+++ ARAP.cpp
@@ -1,11 +1,14 @@
 #include "Eigen.h"
 #include "Mesh.h"
+#include "ARAPInitEnum.h"
 using namespace Eigen;
 
+#define tol 1e-3
+#define alpha 25.0
 
-std::pair<bool, Vector3d> isConstrained(const std::vector<HandlePoint>& C, int index) {
+std::pair<bool, Vector3d> isConstrained(const std::vector<ControlPoint>& C, int index) {
 
-    for (const HandlePoint& c : C) {
+    for (const ControlPoint& c : C) {
         if (index == c.vertexIndexInMesh) {
             return std::pair<bool, Vector3d>(true, c.wantedVertexPosition);
         }
@@ -22,7 +25,8 @@ std::pair<bool, Vector3d> isConstrained(const std::vector<HandlePoint>& C, int i
  * Out : Local rigidity energy
 */
 
-double compute_reg_energy(const Eigen::MatrixXd& Vi, 
+double compute_reg_energy(const Eigen::MatrixXd& W,
+                          const Eigen::MatrixXd& Vi, 
                           const MatrixXd Vi_p, 
                           const std::vector<MatrixXd>& R, 
                           const std::vector<std::list<int>>& N)
@@ -38,13 +42,15 @@ double compute_reg_energy(const Eigen::MatrixXd& Vi,
         {
             VectorXd ni = Vi.row(*it);
             VectorXd ni_p = Vi_p.row(*it);
-            energy +=  pow(((vi - ni) - Ri * (vi_p - ni_p)).norm(), 2);
+             // Weight of the edge
+            double wij = W(i, *it);
+            energy +=  (float)wij * pow(((vi - ni) - Ri * (vi_p - ni_p)).norm(), 2);
         }
     }
     return energy;
 }
 
-double compute_fit_energy(const Eigen::MatrixXd& Vi, const std::vector<HandlePoint>& C, const Eigen::MatrixXd& T)
+double compute_fit_energy(const Eigen::MatrixXd& Vi, const std::vector<ControlPoint>& C)
 {
     double energy = 0;
     for (int i = 0; i <= Vi.rows(); i++)
@@ -60,20 +66,20 @@ double compute_fit_energy(const Eigen::MatrixXd& Vi, const std::vector<HandlePoi
     return energy;
 }
 
-double compute_total_energy(const Eigen::MatrixXd& Vi, 
+double compute_total_energy(const Eigen::MatrixXd& W,
+                            const Eigen::MatrixXd& Vi, 
                             const MatrixXd Vi_p, 
                             const std::vector<MatrixXd>& R, 
                             const std::vector<std::list<int>>& N,
-                            const std::vector<HandlePoint>& C, 
-                            const Eigen::MatrixXd& T)
+                            const std::vector<ControlPoint>& C)
 {
     double total_energy  = 0;
-    total_energy += compute_reg_energy(Vi, Vi_p, R, N);
-    total_energy += compute_fit_energy(Vi, C, T);
+    total_energy += compute_reg_energy(W, Vi, Vi_p, R, N);
+    total_energy += compute_fit_energy(Vi, C);
     return total_energy;
 }
 
-MatrixXd compute_covariance_matrix(const Eigen::MatrixXd& W, 
+MatrixXd compute_covariance_matrix(const Eigen::MatrixXd& W,
                                    const MatrixXd& Vi, 
                                    const MatrixXd& Vi_p, 
                                    const std::vector<std::list<int>>& N, 
@@ -110,7 +116,7 @@ MatrixXd compute_b(const Eigen::MatrixXd& W,
                    const std::vector<std::list<int>>& N, 
                    const MatrixXd& V, 
                    const std::vector<MatrixXd>& R, 
-                   const std::vector<HandlePoint>& C)
+                   const std::vector<ControlPoint>& C)
 {
     MatrixXd b = MatrixXd::Zero(V.rows(),V.rows());
     for (int i = 0; i <= V.rows(); i++)
@@ -167,4 +173,152 @@ MatrixXd compute_Ri(const Eigen::MatrixXd& W,
     DiagonalMatrix<double, 3> D(1, 1, (svd.matrixV() * svd.matrixU().transpose()).determinant());
     MatrixXd Ri = svd.matrixV() * D * svd.matrixU().transpose();
     return Ri;
+}
+
+/* Do the Laplacian initialization
+ * mesh : Initial mesh
+ *
+ * Out : Updated mesh
+ */
+MatrixXd laplacian_init(const Mesh& mesh) {
+    #define V mesh.V
+    #define W mesh.W
+
+    // Get control points
+    const std::vector<ControlPoint>& C = mesh.getControlPoints();
+
+    // Init L
+    MatrixXd laplacian = -W;
+
+    // Add diagonal value
+    for (int i = 0; i < laplacian.rows(); i++) {
+        laplacian(i, i) += -laplacian.row(i).sum();
+    }
+
+    // We want to solve A'Ax = A'(-By + weight_ * p), A and B being a part of L, y the constraint vertices and p the initial vertices
+
+    // Build A, B and y
+    MatrixXd A = MatrixXd::Zero(V.rows(), V.rows() - C.size());
+    MatrixXd B = MatrixXd::Zero(V.rows(), C.size());
+    MatrixXd y = MatrixXd::Zero(C.size(), V.cols());
+
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < V.rows(); i++) {
+        std::pair<bool, Vector3d> constraint = isConstrained(C, i);
+
+        // B
+        if (constraint.first) {
+            B.col(b) = laplacian.col(i);
+            y.row(b) = constraint.second;
+            b++;
+        }
+        // A
+        else {
+            A.col(a) = laplacian.col(i);
+            a++;
+        }
+    }
+
+    // Build A' * A
+    MatrixXd left = A.transpose() * A;
+
+    // Build A' * (-By + L * p)
+    MatrixXd right = A.transpose() * (-B * y + laplacian * V);
+
+    // Solve
+    MatrixXd x = left.ldlt().solve(right);
+
+    // Get the result matrix
+    MatrixXd new_V = MatrixXd::Zero(V.rows(), V.cols());
+    a = 0;
+    for (int i = 0; i < V.rows(); i++) {
+        std::pair<bool, Vector3d> constraint = isConstrained(C, i);
+
+        if (constraint.first) {
+            new_V.row(i) = constraint.second;
+        }
+        else {
+            new_V.row(i) = x.row(a);
+            a++;
+        }
+    }
+
+    return new_V;
+    #undef V
+    #undef W
+}
+
+MatrixXd arap(const Mesh& mesh, const int& kmax, const EInitialisationType& init, int* outInterationNumber = nullptr, float* outInitialEnergy = nullptr, float* outFinalEnergy = nullptr)
+{
+    #define V mesh.V
+    #define N mesh.N
+    #define W mesh.W
+
+    // Get control points
+    const std::vector<ControlPoint>&  C = mesh.getControlPoints();
+
+    // Update L
+    MatrixXd L = mesh.getL_withCP();
+
+    // Init mesh before rotation
+    MatrixXd previous_V = V;
+
+    // Make an intial guess of new_V according to the initialisation type choosen:
+    MatrixXd new_V;
+    // User interaction
+    if (init == EInitialisationType::e_LastFrame) {
+        new_V = V;
+        //std::cout << "Initiated with last frame" << std::endl;
+    }
+    // Laplacian initialization
+    else if (init == EInitialisationType::e_Laplace) {
+        new_V = laplacian_init(mesh);
+        //std::cout << "Initiated with laplacian" << std::endl;
+    }
+
+    // Initialize energies
+    float old_energy = 0;
+    float new_energy = 0;
+
+    int k = 0;
+    do {
+        // Find optimal Ri for each cell
+        std::vector<MatrixXd> R(V.rows()); // Matrix of local rotations
+        for (int i = 0; i < V.rows(); i++) {
+
+            // Compute Ri
+            MatrixXd Ri = compute_Ri(W, previous_V, new_V, N, i);
+
+            // Store Ri
+            R[i] = Ri;
+        }
+        // Compute right-hand side
+        MatrixXd b = compute_b(W, N, previous_V, R, C);
+
+        // Update mesh before rotation
+        previous_V = new_V;
+
+        // Find new optimal mesh
+        new_V = L.ldlt().solve(b);
+
+        // Update energies
+        old_energy = new_energy;
+        new_energy = compute_total_energy(W, previous_V, new_V, R, N, C);
+
+        if (k == 0 && outInitialEnergy != nullptr)
+            *outInitialEnergy = new_energy;
+
+        k++;
+    } while (k < kmax && abs(old_energy - new_energy) > tol);
+
+    if (outInterationNumber != nullptr)
+        *outInterationNumber = k;
+    if (outFinalEnergy != nullptr)
+        *outFinalEnergy = new_energy;
+
+    return new_V;
+    #undef V
+    #undef N
+    #undef W
 }
--- Mesh.h
+++ Mesh.h
@@ -1,5 +1,5 @@
 #include <Eigen/Dense>
-#include "HandlePoint.h"
+#include "ControlPoint.h"
 
 struct Mesh
 {
@@ -10,9 +10,38 @@ struct Mesh
     };
 
 private:
+    std::vector<ControlPoint> C;	// Control points, private because we absolutely want to force the user to user our setter
+	Eigen::MatrixXd L;				// Laplacian Matrix, private because the arap wont access this matrix itself, but a modified version
+
+	void computeN();
+	void computeW();
+	void computeL();
+
 public:
     Eigen::MatrixXd V; // Vertices positions
     Eigen::MatrixXi F; // Faces, defined by vertices' indexes
     void InitMesh(MeshType type);
     void InitMesh(Eigen::MatrixXd V, Eigen::MatrixXi F);
+    std::vector<std::list<int>> N;	// neighboors
+	Eigen::MatrixXd W;				// weight
+
+	Eigen::MatrixXd getVerticesFromIndex(const std::vector<int>& indexes) const;
+
+	const std::vector<ControlPoint>& getControlPoints() const { return C; }
+	std::vector<ControlPoint*> getControlPointsW();
+	std::vector<int> getControlPointsIndex() const;
+	Eigen::MatrixXd getControlPointsWantedPosition() const;
+	Eigen::MatrixXd getControlPointsWantedPositionBySelection(const std::vector<int>& selection, bool invert = false) const;
+	bool isAControlPoint(int vertexIndex) const;
+	ControlPoint* getControlPoint(int vertexIndex);		// be careful : changing C vector may change its memory location => would invalidate the pointer
+	int getControlPointCount() const;
+	
+	void addControlPoint(int vertexIndex);
+	void addControlPoint(int vertexIndex, Eigen::RowVector3d position);
+	void removeControlPoint(int vertexIndex);
+
+	void printControlPoints() const;
+
+	void computeL_W_N();
+	Eigen::MatrixXd getL_withCP() const;
 };
