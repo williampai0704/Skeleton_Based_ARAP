c89429b249fd0bb69c2e07e0dc2f85c9d8fe694e
williampai <williampai0704@gmail.com>
2023-07-22 21:56:00 +0200

    add compute_LBS()

--- main.cpp
+++ main.cpp
@@ -15,6 +15,108 @@ void performARAP(Mesh& mesh, const EInitialisationType& initialisationType, igl:
     viewer.data().set_mesh(mesh.V, mesh.F);
 }
 
+Eigen::Matrix4d compute_trans_matrix(Eigen::Vector3d& vector1, Eigen::Vector3d& vector2)
+{
+    // Step 1: Normalize both vectors
+    vector1.normalize();
+    vector2.normalize();
+
+    if (vector1.norm() == 0 || vector2.norm() == 0) {
+        std::cerr << "Error: Zero vectors are not allowed as input." << std::endl;
+    }
+
+    // Step 2: Find the axis of rotation
+    Eigen::Vector3d axis = vector1.cross(vector2);
+    
+    // Step 3: Check if the input vectors are collinear
+
+    double dotProductVal = vector1.dot(vector2);
+    double angle = std::acos(dotProductVal); // Angle between the vectors
+
+    if (std::isnan(angle)) {
+        // Vectors are collinear
+        if (vector1.isApprox(vector2)) 
+        {
+            // Vectors are identical, no transformation is needed
+            std::cout << "Vectors are identical" << std::endl;
+            // Step 5: Calculate the translation vector
+            Eigen::Vector3d translation = vector2 - vector1;
+            // Step 6: Compute the translation matrix
+            Eigen::Matrix4d translationMatrix = Eigen::Matrix4d::Identity();
+            translationMatrix.block<3, 1>(0, 3) = translation;
+
+            Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+            transformationMatrix(0, 0) = 1.0;
+            transformationMatrix(1, 1) = 1.0;
+            transformationMatrix(2, 2) = 1.0;
+            transformationMatrix.block<3, 1>(0, 3) = translation;
+            return transformationMatrix;
+        } 
+        else 
+        {
+            // Vectors are collinear with different magnitudes
+            // Calculate the scaling factor to transform one vector to the other
+            double scalingFactor = vector2.norm() / vector1.norm();
+
+            // Construct the scaling transformation matrix
+            Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+            transformationMatrix(0, 0) = scalingFactor;
+            transformationMatrix(1, 1) = scalingFactor;
+            transformationMatrix(2, 2) = scalingFactor;
+
+            // Print the transformation matrix
+            std::cout << "Transformation Matrix (Scaling):" << std::endl;
+            std::cout << transformationMatrix << std::endl;
+
+            return transformationMatrix;
+        }
+    }
+    
+    Eigen::Matrix3d skewSymmetricMat;
+    skewSymmetricMat << 0, -axis[2], axis[1],
+                        axis[2], 0, -axis[0],
+                        -axis[1], axis[0], 0;
+
+    Eigen::Matrix3d rotationMat = Eigen::Matrix3d::Identity() +
+                                 skewSymmetricMat * std::sin(angle) +
+                                 (skewSymmetricMat * skewSymmetricMat) * (1 - std::cos(angle));
+
+    // Step 5: Calculate the translation vector
+    Eigen::Vector3d translation = vector2 - vector1;
+
+    // Step 6: Compute the translation matrix
+    Eigen::Matrix4d translationMatrix = Eigen::Matrix4d::Identity();
+    translationMatrix.block<3, 1>(0, 3) = translation;
+
+    // Step 7: Combine the rotation and translation matrices to get the final transformation matrix
+    Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+    transformationMatrix.block<3, 3>(0, 0) = rotationMat;
+    transformationMatrix.block<3, 1>(0, 3) = translation;
+
+    if (transformationMatrix.hasNaN()) {
+        std::cerr << "Error: Computed transformation matrix contains NaN values." << std::endl;
+    }
+    
+    return transformationMatrix;
+    // // Print the transformation matrix
+    // std::cout << "Transformation Matrix:" << std::endl;
+    // std::cout << transformationMatrix << std::endl;
+}
+
+void compute_LSB(Eigen::MatrixXd V_previous, MatrixXd V_after, MatrixXd W)
+{
+    std::vector<Eigen::Matrix4d> LSB;
+    for (int i = 0; i < V_previous.rows()-5; i+= 5)
+    {
+        Eigen::Vector3d previousBone = V_previous.row(i+5) - V_previous.row(i);
+        Eigen::Vector3d newBone = V_after.row(i+5) - V_after.row(i);
+        Eigen::Matrix4d M = compute_trans_matrix(previousBone,newBone);
+        LSB.push_back(M);
+        std::cout << i/5 << std::endl;
+        std::cout << M << std::endl;
+    }
+}
+
 struct Face {
     int v1, v2, v3;
 };
@@ -104,6 +206,7 @@ int main(int argc, char *argv[])
     std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> offFile = read_off();
     mesh.V = get<0>(offFile);
     mesh.F = get<1>(offFile);
+    Eigen::MatrixXd W = read_attachment();
     cout << "Reading complete !" << endl;
 
     // mesh.InitMesh(Mesh::MeshType::SKELETON);
@@ -125,14 +228,17 @@ int main(int argc, char *argv[])
     igl::opengl::glfw::Viewer viewer;
     InterfaceManager interfaceManager = InterfaceManager();
 
-    viewer.callback_pre_draw = [&interfaceManager, &mesh, &needToPerformArap, &initialisationType](igl::opengl::glfw::Viewer& viewer)->bool
+    viewer.callback_pre_draw = [&interfaceManager, &mesh, &needToPerformArap, &initialisationType, &W](igl::opengl::glfw::Viewer& viewer)->bool
     {
         if (needToPerformArap)
         {
-            std::cout << "Current mesh.V: " << mesh.V << std::endl;
+            // std::cout << "Current mesh.V: " << mesh.V << std::endl;
+            Eigen::MatrixXd V_previous = mesh.V;
             performARAP(mesh, initialisationType, viewer, interfaceManager);
+            Eigen::MatrixXd V_after = mesh.V;
             needToPerformArap = false;
-            std::cout << "mesh.V after ARAP: " << mesh.V << std::endl;
+            // std::cout << "mesh.V after ARAP: " << mesh.V << std::endl;
+            compute_LSB(V_previous,V_after,W);
         }
 
         return false;
