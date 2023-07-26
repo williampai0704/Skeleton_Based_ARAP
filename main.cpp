76112635296d8d3207ca45d677899e89db47aa6b
williampai <williampai0704@gmail.com>
2023-07-26 15:50:00 +0200

    add comment

--- main.cpp
+++ main.cpp
@@ -9,66 +9,87 @@
 
 using namespace std;
 
-void performARAP(Mesh &pseudo_mesh, Mesh &bone, const EInitialisationType& initialisationType, igl::opengl::glfw::Viewer& viewer, const InterfaceManager& interfaceManager)
+void performARAP(Mesh &pseudo_mesh, Mesh& bone, const EInitialisationType& initialisationType, igl::opengl::glfw::Viewer& viewer, const InterfaceManager& interfaceManager)
 {
     pseudo_mesh.V = arap(pseudo_mesh, 100, initialisationType);
 }
 
+// Eigen::Matrix4d compute_trans_matrix(Eigen::Vector3d vector1, Eigen::Vector3d vector2)
+// {
+//     double scalingFactor = vector2.norm() / vector1.norm();
+//      Eigen::Vector3d translation = vector2 - vector1;
+//     vector1.normalize();
+//     vector2.normalize();
+//     Eigen::Vector3d axis = vector1.cross(vector2);
+//     double angle = std::acos(vector1.dot(vector2));
+
+//     // Check if the vectors are collinear
+//     if (std::abs(1 - std::abs(vector1.dot(vector2))) < 1e-6) {
+//         // Vectors are collinear with the same direction (parallel)
+//         // Compute the scaling factor
+        
+
+//         // Step 2: Compute the translation vector
+
+//         // Step 3: Create the transformation matrix (scaling and translation)
+//         Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+//         transformationMatrix.block<3, 3>(0, 0) = scalingFactor * Eigen::Matrix3d::Identity();
+//         transformationMatrix.block<3, 1>(0, 3) = translation;
+
+//         // Print the transformation matrix
+//         std::cout << "Transformation Matrix (Collinear with Same Direction):" << std::endl;
+//         std::cout << transformationMatrix << std::endl;
+
+//         return transformationMatrix;
+        
+//     }
+
+//     // Step 4: Compute the rotation matrix using Eigen's built-in function
+//     Eigen::Matrix3d rotationMat = Eigen::AngleAxisd(angle, axis).toRotationMatrix();
+
+//     // Step 5: Compute the translation vector
+   
+
+//     // Step 6: Create the transformation matrix (rotation and translation)
+//     Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+//     transformationMatrix.block<3, 3>(0, 0) = scalingFactor * rotationMat;
+//     transformationMatrix.block<3, 1>(0, 3) = translation;
+//     return transformationMatrix;
+// }
+
+// Compute 4*4 transformation matrix for Linear BLend Skinning
 Eigen::Matrix4d compute_trans_matrix(Eigen::Vector3d vector1, Eigen::Vector3d vector2)
 {
-    // Step 1: Normalize both vectors
-    vector1.normalize();
-    vector2.normalize();
+    Eigen::Vector3d translation = vector2 - vector1;
+    double scalingFactor = vector2.norm() / vector1.norm();
+    Eigen::Vector3d vector1p = vector1;
+    Eigen::Vector3d vector2p = vector2;
 
-    if (vector1.norm() == 0 || vector2.norm() == 0) {
+    // Step 1: Normalize both vectors
+    vector1p.normalize();
+    vector2p.normalize();
+
+    if (vector1p.norm() == 0 || vector2p.norm() == 0) {
         std::cerr << "Error: Zero vectors are not allowed as input." << std::endl;
     }
 
     // Step 2: Find the axis of rotation
-    Eigen::Vector3d axis = vector1.cross(vector2);
+    Eigen::Vector3d axis = vector1p.cross(vector2p);
     
     // Step 3: Check if the input vectors are collinear
 
-    double dotProductVal = vector1.dot(vector2);
+    double dotProductVal = vector1p.dot(vector2p);
     double angle = std::acos(dotProductVal); // Angle between the vectors
 
     if (std::isnan(angle)) {
-        // Vectors are collinear
-        if (vector1.isApprox(vector2)) 
-        {
-            // Vectors are identical, no transformation is needed
-            std::cout << "Vectors are identical" << std::endl;
-            // Step 5: Calculate the translation vector
-            Eigen::Vector3d translation = vector2 - vector1;
-            // Step 6: Compute the translation matrix
-            Eigen::Matrix4d translationMatrix = Eigen::Matrix4d::Identity();
-            translationMatrix.block<3, 1>(0, 3) = translation;
+         // Construct the scaling transformation matrix
+        Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+        transformationMatrix(0, 0) = scalingFactor;
+        transformationMatrix(1, 1) = scalingFactor;
+        transformationMatrix(2, 2) = scalingFactor;
+        transformationMatrix.block<3, 1>(0, 3) = translation;
 
-            Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
-            transformationMatrix(0, 0) = 1.0;
-            transformationMatrix(1, 1) = 1.0;
-            transformationMatrix(2, 2) = 1.0;
-            transformationMatrix.block<3, 1>(0, 3) = translation;
-            return transformationMatrix;
-        } 
-        else 
-        {
-            // Vectors are collinear with different magnitudes
-            // Calculate the scaling factor to transform one vector to the other
-            double scalingFactor = vector2.norm() / vector1.norm();
-
-            // Construct the scaling transformation matrix
-            Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
-            transformationMatrix(0, 0) = scalingFactor;
-            transformationMatrix(1, 1) = scalingFactor;
-            transformationMatrix(2, 2) = scalingFactor;
-
-            // Print the transformation matrix
-            // std::cout << "Transformation Matrix (Scaling):" << std::endl;
-            // std::cout << transformationMatrix << std::endl;
-
-            return transformationMatrix;
-        }
+        return transformationMatrix;
     }
     
     Eigen::Matrix3d skewSymmetricMat;
@@ -76,16 +97,9 @@ Eigen::Matrix4d compute_trans_matrix(Eigen::Vector3d vector1, Eigen::Vector3d ve
                         axis[2], 0, -axis[0],
                         -axis[1], axis[0], 0;
 
-    Eigen::Matrix3d rotationMat = Eigen::Matrix3d::Identity() +
+    Eigen::Matrix3d rotationMat = scalingFactor * Eigen::Matrix3d::Identity() +
                                  skewSymmetricMat * std::sin(angle) +
                                  (skewSymmetricMat * skewSymmetricMat) * (1 - std::cos(angle));
-
-    // Step 5: Calculate the translation vector
-    Eigen::Vector3d translation = vector2 - vector1;
-
-    // Step 6: Compute the translation matrix
-    Eigen::Matrix4d translationMatrix = Eigen::Matrix4d::Identity();
-    translationMatrix.block<3, 1>(0, 3) = translation;
 
     // Step 7: Combine the rotation and translation matrices to get the final transformation matrix
     Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
@@ -108,12 +122,13 @@ void compute_LBS(Eigen::MatrixXd& B_previous, MatrixXd& B_after, MatrixXd& A, Me
     std::vector<Eigen::Matrix4d> LBS;
     for (int i = 0; i < B_previous.rows()-1; ++i)
     {
+        cout << i << endl;
         Eigen::Vector3d previousBone = B_previous.row(i+1) - B_previous.row(i);
         Eigen::Vector3d newBone = B_after.row(i+1) - B_after.row(i);
         Eigen::Matrix4d M = compute_trans_matrix(previousBone,newBone);
         LBS.push_back(M);
-        // std::cout << i << std::endl;
-        // std::cout << M << std::endl;
+        std::cout << i << std::endl;
+        std::cout << M << std::endl;
     }
 
     MatrixXd new_Surface(surface.V.rows(),surface.V.cols());
@@ -123,18 +138,15 @@ void compute_LBS(Eigen::MatrixXd& B_previous, MatrixXd& B_after, MatrixXd& A, Me
         Eigen::Matrix4d L = Matrix4d::Zero();
         for(int j = 0; j < B_previous.rows()-1; ++j)
         {
-            // cout << j << endl;
+            cout << j << endl;
             L += LBS[j]* A(i,j);
         }
-        // cout << L << endl;
         Eigen::Vector3d vec3D = surface.V.row(i);
         Eigen::Vector4d input_temp(vec3D[0],vec3D[1],vec3D[2], 1.0);
         Eigen::Vector4d output_temp = L * input_temp;
         new_Surface.row(i) = output_temp.head<3>();
-        // cout << "finish" << endl;
-        // cout << new_Surface.row(i) << endl;
+
     }
-    // cout << "new surface" << new_Surface << endl;
     surface.V = new_Surface;
 
 }
@@ -143,7 +155,7 @@ struct Face {
     int v1, v2, v3;
 };
 
-// Read vertices and faces 
+// Read pseudo skeleton mesh 
 tuple<Eigen::MatrixXd, Eigen::MatrixXi> read_pseudo_off()
 {
     string filePath = "./../mesh/pseudo_mesh_5.off";
@@ -184,6 +196,7 @@ tuple<Eigen::MatrixXd, Eigen::MatrixXi> read_pseudo_off()
     return std::tuple<Eigen::MatrixXd, Eigen::MatrixXi>{Vi,F};
 }
 
+// Read surface mesh
 tuple<Eigen::MatrixXd, Eigen::MatrixXi> read_surface_off()
 {
     string filePath = "./../mesh/bear_surface.off";
@@ -224,6 +237,7 @@ tuple<Eigen::MatrixXd, Eigen::MatrixXi> read_surface_off()
     return std::tuple<Eigen::MatrixXd, Eigen::MatrixXi>{Vi,F};
 }
 
+// Read bone indices
 tuple<Eigen::MatrixXd, Eigen::MatrixXd> get_Bone(Eigen::MatrixXd V)
 {
     string filePath = "./../mesh/pinocchino_match.txt";
@@ -247,6 +261,7 @@ tuple<Eigen::MatrixXd, Eigen::MatrixXd> get_Bone(Eigen::MatrixXd V)
     return std::tuple<Eigen::MatrixXd, Eigen::MatrixXd>{B, Bi};
 }
 
+// Read coefficient for Linear Blend Skinning
 Eigen::MatrixXd read_attachment()
 {
     string filePath = "./../mesh/attachment_2.out";
@@ -287,10 +302,15 @@ Eigen::MatrixXd read_attachment()
 
 int main(int argc, char *argv[])
 {
+    // Initialize meshes
+    // pseudo_mesh = pseudo joints including bones
+    // surface = surface mesh 
+    // bone = bone vertices
     Mesh pseudo_mesh = Mesh();
     Mesh surface = Mesh();
     Mesh bone = Mesh();
 
+    // Get mesh by reading files
     std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> poffFile = read_pseudo_off();
     pseudo_mesh.V = get<0>(poffFile);
     pseudo_mesh.F = get<1>(poffFile);
@@ -298,6 +318,10 @@ int main(int argc, char *argv[])
     std::tuple<Eigen::MatrixXd, Eigen::MatrixXi> offFile = read_surface_off();
     surface.V = get<0>(offFile);
     surface.F = get<1>(offFile);
+
+    tuple<Eigen::MatrixXd, Eigen::MatrixXd> bone_tuple = get_Bone(pseudo_mesh.V);
+    bone.V = get<0>(bone_tuple);
+    Eigen::MatrixXd bone_index = get<1>(bone_tuple);
 
     Eigen::MatrixXd A = read_attachment();
     
@@ -309,15 +333,10 @@ int main(int argc, char *argv[])
     // pseudo_mesh.V = pseudo_mesh.V.rowwise() - pseudo_mesh.V.colwise().mean();
     // surface.V = surface.V.rowwise() - surface.V.colwise().mean();
 
-    tuple<Eigen::MatrixXd, Eigen::MatrixXd> bone_tuple = get_Bone(pseudo_mesh.V);
-    bone.V = get<0>(bone_tuple);
-    Eigen::MatrixXd bone_index = get<1>(bone_tuple);
-
     cout << pseudo_mesh.V.rows() << " vertices loaded." << endl;
     cout << pseudo_mesh.F.rows() << " faces loaded" << endl;
     cout << surface.V.rows() << " vertices loaded." << endl;
     cout << surface.F.rows() << " faces loaded" << endl;
-    // cout << A.rows() << " " <<  A.cols() << " attachments loaded." << endl;
 
     bool needToPerformArap = false;
     EInitialisationType initialisationType = EInitialisationType::e_LastFrame;
